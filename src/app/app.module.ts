634af56e9ebae9b080c57c6a98ccd135b851e59a
Matteo Zaccagnino <matteozaccagnino@mail.com>
2018-01-24 19:53:23 +0100

    added page structure stub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { ResultsComponent } from './components/results/results.component';
+import { SimulationsComponent } from './components/simulations/simulations.component';
 
+const routes: Routes = [
+  { path: 'about', component: AboutComponent, data: { title: 'Wgida/About' } },
+  { path: 'results', component: ResultsComponent, data: { title: 'Wgida/Results' } },
+  { path: 'simulations', component: SimulationsComponent, data: { title: 'Wgida/Simulations' } },
+  { path: '**', redirectTo: 'about' },
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    ResultsComponent,
+    SimulationsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, { useHash: true }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/simulations/simulations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simulations',
+  templateUrl: './simulations.component.html',
+  styleUrls: ['./simulations.component.scss']
+})
+export class SimulationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
